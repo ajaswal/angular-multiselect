1a3fa61065db7d1c0d14a42ba5671a2da16304a3
Ajay Aswal <aswal.ajay@hotmail.com>
2015-03-25 15:00:18 +0530

    Full Width

--- src/angularjs-dropdown-multiselect.js
+++ src/angularjs-dropdown-multiselect.js
@@ -21,7 +21,7 @@ directiveModule.directive('ngDropdownMultiselect', ['$filter', '$document', '$co
                 var checkboxes = attrs.checkboxes ? true : false;
                 var groups = attrs.groupBy ? true : false;
 
-                var template = '<div class="multiselect-parent btn-group dropdown-multiselect">';
+                var template = '<div class="multiselect-parent btn-group dropdown-multiselect" style="width:100%">';
                 template += '<button type="button" class="btn-custom-select dropdown-toggle" ng-class="settings.buttonClasses" ng-click="toggleDropdown()" ng-disabled="ngDisabled" style="text-align:left;">{{getButtonText()}}&nbsp;<span class="glyphicon glyphicon-chevron-down chevron-align"></span></button>';
                 template += '<ul class="dropdown-menu dropdown-menu-form" ng-style="{display: open ? \'block\' : \'none\', height : settings.scrollable ? settings.scrollableHeight : \'auto\' }" style="overflow: scroll" >';
                 template += '<li ng-hide="!settings.showCheckAll || settings.selectionLimit > 0"><a data-ng-click="selectAll()"><span class="glyphicon glyphicon-ok green-ok"></span>  {{texts.checkAll}}</a>';
